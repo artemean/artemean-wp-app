9e0a5a94140019dc7b6b9b0fad89a7a95cd11c08
Andrii Artemenko1 <Andrii_Artemenko1@epam.com>
2018-02-21 10:34:52 +0100

    Add header and footer to PostPage

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import Post from "../components/Post/Post";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 export default class PostPage extends Component {
   render() {
     const postIdFromUrl = this.props.match.params.postId;
     return (
       <div>
+        <Header
+          sitename="Artemean Blog"
+          description="Just another WordPress site"
+        />
         <Post postId={postIdFromUrl} />
+        <Footer />
       </div>
     );
   }
