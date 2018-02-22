4c45703f89d705785139f2301f9d25144b825e4a
Andrii Artemenko1 <Andrii_Artemenko1@epam.com>
2018-02-22 12:57:41 +0100

    Added react-refetch
    * refactor home page for react-refetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,29 @@
 import React, { Component } from "react";
+import { connect } from "react-refetch";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import ExcerptList from "../components/ExcerptList/ExcerptList";
 import PostsPlaceholder from "../components/ExcerptList/ExcerptsPlaceholder";
 
-export default class Home extends Component {
-  state = {
-    posts: [],
-  };
-
-  componentDidMount() {
-    fetch("http://wp-api.artemean.com/wp-json/wp/v2/posts")
-      .then(res => res.json())
-      .then(posts => {
-        this.setState({
-          posts,
-        });
-      });
-  }
-
+class Home extends Component {
   render() {
-    const MainBlock = this.state.posts.length ? (
-      <ExcerptList posts={this.state.posts} />
-    ) : (
-      <PostsPlaceholder />
-    );
+    const { postsFetch } = this.props;
+    const MainBlock = () => {
+      // render the different promise states
+      if (postsFetch.pending) {
+        return <PostsPlaceholder />;
+      } else if (postsFetch.rejected) {
+        return <div>Error!</div>;
+      } else if (postsFetch.fulfilled) {
+        const posts = postsFetch.value;
+
+        return (
+          <div>
+            <ExcerptList posts={posts} />
+          </div>
+        );
+      }
+    };
 
     return (
       <div>
@@ -32,9 +31,15 @@ export default class Home extends Component {
           sitename="Artemean Blog"
           description="Just another WordPress site"
         />
-        {MainBlock}
+        {MainBlock()}
         <Footer />
       </div>
     );
   }
 }
+
+export default connect(props => {
+  return {
+    postsFetch: "http://wp-api.artemean.com/wp-json/wp/v2/posts",
+  };
+})(Home);
